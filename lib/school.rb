102d6abd9bf1570153ee3333f9a388f5faa79638
Samiylo Kryshu <skryshu@gmail.com>
2020-04-01 23:00:36 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+
+class School
+  
+  
+  def initialize(name)
+    @name = name
+    @roster = Hash.new{ |h, k| h[k] = [] }
+  end
+  
+  def roster 
+    @roster
+  end
+  
+  def add_student(student_name, grade)
+    @roster[grade] << student_name
+    
+  end
+  
+  def grade(grade)
+    @grade = grade
+    @roster[@grade]
+  end
+  
+  def sort
+    @result = {}
+    @roster.each do |key, value|
+      @key = key
+      @value = value
+      # @result[@key] = []
+      @result[@key] = @value.sort 
+    end
+    @result
+    
+  end
+  
+end
